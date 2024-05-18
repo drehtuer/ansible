ededaffa787e59a67d8d8e6cc03e699ed118c55e
drehtuer <drehtuer@drehtuer.de>
2024-05-18 20:46:51 +0000

    Added invoke, ruff, ansible-lint, sshpass

---
+++ tasks.py
@@ -0,0 +1,21 @@
+"""
+https://docs.pyinvoke.org/en/stable/
+"""
+from invoke import task
+
+ANSIBLE = 'ansible'
+INVENTORY = 'inventory.ini'
+
+
+@task
+def ssh(c, host):
+  c.run(f'ssh {host}', pty=True)
+
+
+@task
+def ping(c, hosts, askpass=False):
+  cmd = f'{ANSIBLE} --module-name ping --inventory {INVENTORY} {hosts}'
+  if askpass:
+    cmd += ' --ask-pass'
+  print(cmd)
+  c.run(cmd)
